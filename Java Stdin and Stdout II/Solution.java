698988c9b5ae9b71a621fb5a3d92e1417e5e69bf
SathvikSesha <lakshmisarmatk@gmail.com>
2025-05-19 16:52:06 +0530

    Solutions are here

--- Java Stdin and Stdout II/Solution.java
+++ Java Stdin and Stdout II/Solution.java
@@ -1,22 +1,17 @@
-import java.io.*;
+import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(bufferedReader.readLine().trim());
-        if (N % 2 != 0) {
-            System.out.println("Weird");
-        } else {
-            if (N >= 2 && N <= 5) {
-                System.out.println("Not Weird");
-            } else if (N >= 6 && N <= 20) {
-                System.out.println("Weird");
-            } else if (N > 20) {
-                System.out.println("Not Weird");
-            }
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        double b = sc.nextDouble();
+        sc.nextLine();
+        String str = sc.nextLine();
+        System.out.println("String: " + str);
+        System.out.println("Double: " + b);
+        System.out.println("Int: " + a);
+        sc.close();
 
-        bufferedReader.close();
     }
 }
