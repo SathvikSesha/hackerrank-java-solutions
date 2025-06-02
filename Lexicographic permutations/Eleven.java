556e41958357dbda830eb5b5acf3ed1fd837b2b4
SathvikSesha <lakshmisarmatk@gmail.com>
2025-06-02 15:51:26 +0530

    new solution

--- Lexicographic permutations/Eleven.java
+++ Lexicographic permutations/Eleven.java
@@ -1,63 +1,45 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Eleven {
+    static String getKthPermutation(String str, int k) {
+        List<Character> chars = new ArrayList<>();
+        for (char c : str.toCharArray()) {
+            chars.add(c);
+        }
+
+        int n = chars.size();
+        long[] factorial = new long[n + 1];
+        factorial[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+
+        if (k > factorial[n]) {
+            return "No more permutations possible.";
+        }
+
+        k--; // convert to 0-based index
+        StringBuilder result = new StringBuilder();
+
+        for (int i = n; i > 0; i--) {
+            int index = (int) (k / factorial[i - 1]);
+            result.append(chars.get(index));
+            chars.remove(index);
+            k %= factorial[i - 1];
+        }
+
+        return result.toString();
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // Number of test cases
+        int t = sc.nextInt(); // number of test cases
 
-        for (int j = 0; j < n; j++) {
-            int k = sc.nextInt(); // Number of permutations
+        for (int j = 0; j < t; j++) {
+            int k = sc.nextInt(); // find the k-th permutation
             String str = "abcdefghijklm";
-            char[] arr = str.toCharArray();
-            boolean valid = true;
-
-            for (int q = 1; q < k; q++) {
-                int index = -1;
-
-                // Step 1: Find the index where arr[i] < arr[i + 1]
-                for (int i = arr.length - 2; i >= 0; i--) {
-                    if (arr[i] < arr[i + 1]) {
-                        index = i;
-                        break;
-                    }
-                }
-
-                // No more permutations possible
-                if (index == -1) {
-                    valid = false;
-                    break;
-                }
-
-                // Step 2: Find the smallest character greater than arr[index]
-                int smallest = index + 1;
-                for (int i = index + 1; i < arr.length; i++) {
-                    if (arr[i] > arr[index] && arr[i] <= arr[smallest]) {
-                        smallest = i;
-                    }
-                }
-
-                // Step 3: Swap
-                char temp = arr[index];
-                arr[index] = arr[smallest];
-                arr[smallest] = temp;
-
-                // Step 4: Reverse suffix
-                int left = index + 1, right = arr.length - 1;
-                while (left < right) {
-                    char t = arr[left];
-                    arr[left] = arr[right];
-                    arr[right] = t;
-                    left++;
-                    right--;
-                }
-            }
-
-            if (valid) {
-                System.out.println(new String(arr));
-            } else {
-                System.out.println("No more permutations possible.");
-            }
+            System.out.println(getKthPermutation(str, k));
         }
-        sc.close();
     }
 }
